18278f6358b478de0693806730800d7f535d10d4
Fred Gu <fredera92@gmail.com>
2026-07-19 15:23:18 +0800

    refactor: 删后端死 controller(ActionEditor/CrosstabEditor)+ VariableController 死端点
    前端老编辑器已全部删除(B2/B6),这三个端点 0 调用方:
    - ActionEditorController(/actioneditor/loadMethods,反射 getBean 执行面)
    - CrosstabEditorController(/crosstabeditor/importExcel)+ 独占的 servlet/crosstab 包 + CellContent
    - VariableController.generateFields(Class.forName 反射面);generateVariableLibrary 保留
      (资源编辑器仍在用)
    mvn compile -pl app/ruleforge-console-app -am 通过

--- server/app/ruleforge-console-app/src/main/java/com/ruleforge/console/controller/VariableController.java
+++ server/app/ruleforge-console-app/src/main/java/com/ruleforge/console/controller/VariableController.java
@@ -1,17 +1,12 @@
 package com.ruleforge.console.controller;
 
-import com.ruleforge.ClassUtils;
 import com.ruleforge.console.repository.ExternalRepository;
-import com.ruleforge.model.library.variable.Variable;
 import com.ruleforge.model.library.variable.VariableLibrary;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -20,15 +15,6 @@ import java.util.List;
 public class VariableController {
 
     private final ExternalRepository externalRepository;
-
-    @PostMapping("/generateFields")
-    public List<Variable> generateFields(@RequestParam String clazz) {
-        try {
-            return ClassUtils.classToVariables(Class.forName(clazz));
-        } catch (ClassNotFoundException e) {
-            return this.externalRepository.generalEntityToVariables(clazz);
-        }
-    }
 
     @PostMapping("/generateVariableLibrary")
     public VariableLibrary generateVariableLibrary() {
--- server/app/ruleforge-console-app/src/main/java/com/ruleforge/console/servlet/crosstab/Type.java
+++
@@ -1,6 +0,0 @@
-package com.ruleforge.console.servlet.crosstab;
-
-public enum Type {
-    left,
-    top;
-}
